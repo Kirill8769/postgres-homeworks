65a3758fa0297d1d2dab877a3004e54ef4e0aa68
kirill8769 <tkkrozn@gmail.com>
2024-02-07 23:04:01 +0300

    finished taks1

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,7 +1,21 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT
+	cust.company_name,
+    CONCAT(e.first_name, ' ', e.last_name) AS employee
+FROM orders o
+	JOIN customers cust USING(customer_id)
+	JOIN employees e USING(employee_id)
+WHERE
+    cust.city = 'London'
+    AND e.city = 'London'
+    AND o.ship_via = (
+        SELECT shipper_id
+        FROM shippers
+        WHERE company_name = 'United Package'
+    )
+ORDER BY 1 DESC, 2 DESC;
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
